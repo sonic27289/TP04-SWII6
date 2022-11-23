c4ad8305e048b169936e2249a63eae292b62dcce
sonic27289 <sonic27289@gmail.com>
2022-11-22 23:20:31 -0300

    TP04-SWII6

---
+++ WideWorldImporters.API/WideWorldImporters.API.UnitTests/WideWorldImporters.API.UnitTests/DbContextMocker.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WideWorldImporters.API.Models;
+
+namespace WideWorldImporters.API.UnitTests
+{
+    public static class DbContextMocker
+    {
+        public static WideWorldImportersDbContext
+            GetWideWorldImportersDbContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<WideWorldImportersDbContext>().UseInMemoryDatabase(databaseName: dbName).Options;
+
+            var dbContext = new WideWorldImportersDbContext(options);
+            dbContext.Seed();
+            return dbContext;
+        }
+    }
+}
